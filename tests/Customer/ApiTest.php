f8a59adf521d9bee42b4e568deb24d605a1b4dad
railken <railken@protonmail.ch>
2018-09-09 22:57:49 +0200

    better tests

--- tests/Customer/ApiTest.php
+++ tests/Customer/ApiTest.php
@@ -3,52 +3,50 @@
 namespace Railken\LaraOre\Tests\Customer;
 
 use Illuminate\Support\Facades\Config;
-use Railken\Bag;
 use Railken\LaraOre\Address\AddressFaker;
 use Railken\LaraOre\Address\AddressManager;
-use Railken\LaraOre\Api\Support\Testing\TestableTrait;
+use Railken\LaraOre\Api\Support\Testing\TestableBaseTrait;
 use Railken\LaraOre\Customer\CustomerFaker;
 use Railken\LaraOre\Customer\CustomerManager;
 
 class ApiTest extends BaseTest
 {
-    use TestableTrait;
+    use TestableBaseTrait;
 
     /**
-     * Retrieve basic url.
+     * Faker class.
      *
-     * @return string
+     * @var string
      */
-    public function getBaseUrl()
-    {
-        return Config::get('ore.api.http.admin.router.prefix').Config::get('ore.customer.http.admin.router.prefix');
-    }
+    protected $faker = CustomerFaker::class;
 
     /**
-     * Test common requests.
+     * Router group resource.
+     *
+     * @var string
      */
-    public function testSuccessCommon()
-    {
-        $this->commonTest($this->getBaseUrl(), CustomerFaker::make()->parameters());
-    }
+    protected $group = 'admin';
 
+    /**
+     * Base path config.
+     *
+     * @var string
+     */
+    protected $config = 'ore.customer';
+
+    /**
+     * Address tests.
+     */
     public function testSuccessAddress()
     {
         $customer = (new CustomerManager())->create(CustomerFaker::make()->parameters())->getResource();
         $url = Config::get('ore.api.http.admin.router.prefix').Config::get('ore.customer.http.admin.router.prefix').'/'.$customer->id.'/addresses';
 
-        // GET /
-        $response = $this->get($url, []);
-        $this->assertOrPrint('GET', $url, new Bag(), $response, 200);
+        $this->callAndTest('GET', $url, [], 200);
 
         $address = (new AddressManager())->create(AddressFaker::make()->parameters())->getResource();
 
-        // POST
-        $response = $this->post($url.'/'.$address->id, []);
-        $this->assertOrPrint('POST', $url.'/'.$address->id, new Bag(), $response, 200);
-
-        // DELETE
-        $response = $this->delete($url.'/'.$address->id, []);
-        $this->assertOrPrint('DELETE', $url.'/'.$address->id, new Bag(), $response, 200);
+        $this->callAndTest('POST', $url.'/'.$address->id, [], 200);
+        $this->callAndTest('DELETE', $url.'/'.$address->id, [], 200);
     }
 }
