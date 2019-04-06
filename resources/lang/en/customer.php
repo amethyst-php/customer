695de9894b3f2cba98674cd8cccc1d0250e92432
railken <railken@protonmail.ch>
2019-04-06 22:40:54 +0200

    add lang

---
+++ resources/lang/en/customer.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Customer',
+    'description' => 'A customer is anyone paying for a good or service. Without customers, businesses would go out of business.',
+    'attributes' => [
+        'legal_entity_id' => [
+            'label' => 'Legal Entity',
+            'description' => 'Legal Entity'
+        ]
+    ]
+]
---
+++ resources/lang/it/customer.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Cliente',
+    'description' => 'Cliente',
+    'attributes' => [
+        'legal_entity_id' => [
+            'label' => 'Legal Entity',
+            'description' => 'Legal Entity'
+        ]
+    ]
+]
