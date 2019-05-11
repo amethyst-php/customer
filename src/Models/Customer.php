eea38942fa71b137aeab9b30386857200b5ae9fc
railken <railken@protonmail.ch>
2019-05-11 17:02:09 +0200

    fix style

--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -12,7 +12,8 @@ use Railken\Lem\Contracts\EntityContract;
 
 class Customer extends Model implements EntityContract
 {
-    use SoftDeletes, ConfigurableModel;
+    use SoftDeletes;
+    use ConfigurableModel;
 
     /**
      * Create a new Eloquent model instance.
